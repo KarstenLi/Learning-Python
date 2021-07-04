acf783bc2ed37b6dc123a793777c83ca3a3729e1
KarstenLi <86924657+KarstenLi@users.noreply.github.com>
2021-07-04 13:13:46 -0400

    Name introduction.

---
+++ nameIntro.py
@@ -0,0 +1,13 @@
+'''
+hello = input ("hello")
+monkey = 'hi'
+
+if hello == "monkey":
+    print ("BANANA")
+    
+else:
+    print ("You dissapoint me.")
+'''
+
+name = input ("Hi, what is your name?")
+print ("That is the worst name ever, " + name + ".")
